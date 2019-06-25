e78f882c052b875fc5d7c5b843b409c4d29c90bd
gugu <gustavocorrea2032@gmail.com>
2019-06-25 12:30:05 -0300

    While1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace While1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            //Realizar uma contagem de 1 a 10
+
+            while (n <=10){
+
+                Console.WriteLine ("Estou em " +n);
+                n ++;
+
+            }
+
+
+        }
+    }
+}
